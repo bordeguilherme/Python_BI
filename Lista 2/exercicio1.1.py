86c483513eaf3452ea2bf3fbce4b46ae7576845f
unknown <guilhermeborde99@gmail.com>
2024-09-24 14:28:27 -0300

    feat: exercicio1.1 da lista 2

--- Lista 2/exercicio1.1.py
+++ Lista 2/exercicio1.1.py
@@ -3,3 +3,9 @@
 # Baixe um arquivo CSV com dados simples
 # (como uma planilha de vendas, clima, ou dados financeiros). 
 # Use a biblioteca pandas para ler o arquivo e exibir as primeiras linhas.
+
+import pandas as pd
+
+arquivo_csv = './vendas_1.csv'
+df = pd.read_csv(arquivo_csv)
+print(df.head())
